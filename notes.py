d387f745a77ee2faf221a8f314bc2e76c4fb68fe
trustinveritas <worlds.ducts.0h@icloud.com>
2023-07-07 17:14:32 +0200

    New Notes

---
+++ notes.py
@@ -0,0 +1,21 @@
+print(255 % 7)
+
+import math
+
+result = math.fmod(255,7)
+print(result)
+print(255**7)
+
+
+myvar = 40
+myvar += 55
+
+print(myvar)
+
+myvar = 40
+myvar = myvar + 55
+
+print(myvar)
+
+
+print("5 +" + 2 + 5.5)
